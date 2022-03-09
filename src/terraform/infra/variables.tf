bd8e29a56ffece64c9f18ac147a8c0a407bf97fd
Cees van de Griend <cees.van.de.griend@xantara-it.nl>
2022-03-09 20:53:56 +0100

    Started on module infra

---
+++ src/terraform/infra/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Location of the Azure Cloud"
+  default     = "westeurope"
+}
+
+variable "rg_name" {
+  description = "Name of the resource group"
+  default     = "rg-infra"
+}
